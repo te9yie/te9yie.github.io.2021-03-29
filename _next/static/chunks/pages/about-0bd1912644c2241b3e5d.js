bc55af1175a6c6b986301fca7d29fbd13505ef43
te9yie <te9yie@users.noreply.github.com>
2021-02-27 09:47:46 +0000

    deploy: 606a224b69568c762a0ed919816833148442c761

--- _next/static/chunks/pages/about-0bd1912644c2241b3e5d.js
+++
@@ -1,1 +0,0 @@
-_N_E=(window.webpackJsonp_N_E=window.webpackJsonp_N_E||[]).push([[7],{TaPz:function(n,t,o){"use strict";o.r(t);var u=o("nKUr"),i=o("xoCZ");t.default=function(){return Object(u.jsx)(i.a,{title:"About",children:Object(u.jsx)("p",{children:"World"})})}},hIud:function(n,t,o){(window.__NEXT_P=window.__NEXT_P||[]).push(["/about",function(){return o("TaPz")}])}},[["hIud",0,2,1,3]]]);
---
+++ _next/static/chunks/pages/about-68ea2ec215691067d006.js
@@ -0,0 +1,1 @@
+_N_E=(window.webpackJsonp_N_E=window.webpackJsonp_N_E||[]).push([[7],{TaPz:function(n,t,o){"use strict";o.r(t);var u=o("nKUr"),e=o("xoCZ");t.default=function(){return Object(u.jsx)(e.a,{title:"About",children:Object(u.jsx)("p",{children:"Hello"})})}},hIud:function(n,t,o){(window.__NEXT_P=window.__NEXT_P||[]).push(["/about",function(){return o("TaPz")}])}},[["hIud",0,2,1,3]]]);
