6da4de06fb5e2b3c323c4e29e23edcffacbbfb95
te9yie <te9yie@users.noreply.github.com>
2021-02-28 23:06:32 +0900

    Add Wiki index

--- pages/w/index.tsx
+++ pages/w/index.tsx
@@ -1,9 +1,25 @@
+import { GetStaticProps } from "next";
 import Body from "../../components/Body";
+import Markdown from "../../components/Markdown";
+import { getWikiData, WikiData } from "../../libs/wiki";
 
-const IndexPage: React.FC = () => (
+const INDEX_MD: string = "@index";
+
+type Props = {
+  data: WikiData;
+};
+
+const IndexPage: React.FC<Props> = ({ data }) => (
   <Body title="Wiki" show_title={false}>
-    <p>なんか色々書いておくところ</p>
+    <article>
+      <Markdown content={data.content} />
+    </article>
   </Body>
 );
+
+export const getStaticProps: GetStaticProps = async () => {
+  const data = await getWikiData(INDEX_MD);
+  return { props: { data } };
+};
 
 export default IndexPage;
