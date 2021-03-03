bc294fc9d59fdc95ddd0dd8bbf2aee307a6300da
te9yie <te9yie@users.noreply.github.com>
2021-03-03 17:38:53 +0900

    Add SearchBox

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,31 +1,31 @@
 import React from "react";
 import Link from "next/link";
+import SearchBox from "./SearchBox";
 import { SITE_NAME } from "../libs/const";
 
-const Nav: React.FC = () => {
-  return (
-    <nav>
-      <ul>
-        <li>
-          <Link href="/">
-            <a id="site-name">
-              <h1>{SITE_NAME}</h1>
-            </a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/b">
-            <a>Blog</a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/w">
-            <a>Wiki</a>
-          </Link>
-        </li>
-      </ul>
-    </nav>
-  );
-};
+const Nav: React.FC = () => (
+  <nav>
+    <ul>
+      <li>
+        <Link href="/">
+          <a id="site-name">
+            <h1>{SITE_NAME}</h1>
+          </a>
+        </Link>
+      </li>
+      <li>
+        <Link href="/b">
+          <a>Blog</a>
+        </Link>
+      </li>
+      <li>
+        <Link href="/w">
+          <a>Wiki</a>
+        </Link>
+      </li>
+    </ul>
+    <SearchBox />
+  </nav>
+);
 
 export default Nav;
---
+++ components/SearchBox.tsx
@@ -0,0 +1,22 @@
+import React, { useEffect } from "react";
+
+const SearchBox: React.FC = () => {
+  const searchBoxRef = React.createRef<HTMLDivElement>();
+  useEffect(() => {
+    if (searchBoxRef && searchBoxRef.current) {
+      const searchBox = searchBoxRef.current;
+      const parent = searchBox.parentNode;
+      const gcse = document.createElement("script");
+      gcse.async = true;
+      gcse.src = "https://cse.google.com/cse.js?cx=c961937a8b4f96e3d";
+      parent?.insertBefore(gcse, searchBox);
+    }
+  });
+  return (
+    <div className="search-box">
+      <div ref={searchBoxRef} className="gcse-search"></div>
+    </div>
+  );
+};
+
+export default SearchBox;
